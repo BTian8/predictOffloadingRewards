5b4a8c81d5e464ee11c070bc68cd6b9e69b8c3f6
BTian8 <boyan.t@wustl.edu>
2022-08-05 00:15:36 -0500

    net

--- net.py
+++ net.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -16,4 +17,19 @@ class Net(nn.Module):
         out = F.sigmoid(out)
         out = self.predict(out)
 
+        return out
+
+
+class Net1(nn.Module):
+    def __init__(self, n_input, n_hidden, n_output, stride):
+        super(Net1, self).__init__()
+        self.conv1 = nn.Conv3d(n_input, n_hidden, stride=stride)
+        # self.dense1 = nn.Linear(n_hidden, n_hidden)
+        self.dense2 = nn.Linear(n_hidden, n_output)
+
+    def forward(self, input):
+        out = F.max_pool3d(input)
+        # out = out.view(data size)
+        out = F.relu(self.dense1(out))
+        out = self.dense2(out)
         return out
