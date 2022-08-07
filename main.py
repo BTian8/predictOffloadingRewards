8fa354fa2c96818f959f3159cf802090c43a5eb9
Jiaming Qiu <qiujiaming@wustl.edu>
2022-08-07 13:04:00 -0500

    implement roi pooling

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import argparse
 import os
 from dataclasses import dataclass
 from pathlib import Path
+import matplotlib.pyplot as plt
 
-import skimage
+import torch
+from torchvision.ops import roi_align, roi_pool
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.linear_model import BayesianRidge, LinearRegression, ElasticNet
 from sklearn.svm import SVR
@@ -12,25 +14,72 @@ from sklearn.svm import SVR
 """Train a regression model that maps the weak detector's intermediate feature map to the offloading reward."""
 
 
-def load_feature(path, stage):
+def roi_padding(x):
+    """
+    Pad the feature map and set it as an roi for pooling.
+    :param x: the feature map for padding.
+    :return: the feature map and its coordinates.
+    """
+    c, h, w = x.shape
+    if h < w:
+        padding = ((0, 0), (0, w - h), (0, 0))
+    else:
+        padding = ((0, 0), (0, 0), (0, h - w))
+    # Padding value does not matter, use the default zero padding mode.
+    feature_map = np.pad(x, padding)
+    coord = [[0, 0, w, h]]
+    return feature_map, coord
+
+
+def load_feature(path, stage, pool=True, batch_size=128, func="avg", size=4):
     """
     Load the feature maps.
     :param path: path to the folder where the feature maps are stored.
     :param stage: the stage number of the feature map.
-    :return: the loaded data as an ndarray.
+    :param pool: whether the feature maps should be resized with roi pooling.
+    :param batch_size: batch size for pooling image, only active when "pool" is enabled.
+    :param func: function for pooling image, "avg" or "max", only active when "pool" is enabled.
+    :param size: size (H, W) of the feature map after pooling, only active when "pool" is enabled.
+    :return: the loaded (pooled) feature maps as a list or ndarray.
     """
     # The stage names of yolov5 detectors.
     v5_names = ['Conv', 'Conv', 'C3', 'Conv', 'C3', 'Conv', 'C3', 'Conv', 'C3', 'SPPF', 'Conv', 'Upsample', 'Concat',
                 'C3', 'Conv', 'Upsample', 'Concat', 'C3', 'Conv', 'Concat', 'C3', 'Conv', 'Concat', 'C3']
     data = list()
     # Read the data from each npy file.
-    for img_name in os.listdir(path):
-        file_path = os.path.join(path, img_name, f"stage{stage}_{v5_names[stage]}_features.npy")
-        file_data = np.load(file_path)
-        # Down-sample the feature maps to reduce data size.
-        # TODO: Play around with different choices of block size and down sampling functions.
-        file_data = skimage.measure.block_reduce(file_data, block_size=(1, 2, 2), func=np.max)
-        data.append(file_data)
+    images = [f for f in os.listdir(path) if not os.path.isfile(os.path.join(path, f))]
+    if pool:
+        pool_func = roi_align if func == "avg" else roi_pool
+        # Split images into batches and resize the feature maps through roi pooling.
+        for ndx in range(0, len(images), batch_size):
+            features, coords = list(), list()
+            for img_name in images[ndx:min(ndx + batch_size, len(images))]:
+                file_path = os.path.join(path, img_name, f"stage{stage}_{v5_names[stage]}_features.npy")
+                file_data = np.load(file_path)
+                # Pad the feature maps to set the original feature map as an roi for the pooling operation.
+                feature_map, coord = roi_padding(file_data)
+                features.append(feature_map)
+                coords.append(torch.tensor(coord, dtype=torch.float))
+            data.append(pool_func(torch.from_numpy(np.array(features)), coords, size).numpy())
+        data = np.concatenate(data)
+        # # Visualize the feature maps.
+        # for img_name, feature_map in zip(images, data):
+        #     file_path = os.path.join(path, img_name, f"stage{stage}_{v5_names[stage]}_features_pooled.png")
+        #     n = 32
+        #     fig, ax = plt.subplots(4, 8, tight_layout=True)
+        #     ax = ax.ravel()
+        #     plt.subplots_adjust(wspace=0.05, hspace=0.05)
+        #     for i in range(n):
+        #         ax[i].imshow(feature_map[i])
+        #         ax[i].axis('off')
+        #     plt.savefig(file_path, dpi=300, bbox_inches='tight')
+        #     plt.close()
+    else:
+        # Directly load the feature maps without modification.
+        for img_name in images:
+            file_path = os.path.join(path, img_name, f"stage{stage}_{v5_names[stage]}_features.npy")
+            file_data = np.load(file_path)
+            data.append(file_data)
     return data
 
 
@@ -106,7 +155,7 @@ def fit_SVR(train_feature, val_feature, train_reward, opts=_SVROPT):
     """Fit a support vector regression model to predict offloading reward."""
     train_feature = [x.flatten() for x in train_feature]
     val_feature = [x.flatten() for x in val_feature]
-    reg = SVR(kernel=opts.kernel, gamma=opts.gamma, C=C).fit(train_feature, train_reward)
+    reg = SVR(kernel=opts.kernel, gamma=opts.gamma, C=opts.C).fit(train_feature, train_reward)
     train_est, val_est = reg.predict(train_feature), reg.predict(val_feature)
     return train_est, val_est
 
@@ -127,15 +176,15 @@ def fit_GBR(train_feature, val_feature, train_reward, opts=_GBROPT):
     train_feature = [x.flatten() for x in train_feature]
     val_feature = [x.flatten() for x in val_feature]
     reg = GradientBoostingRegressor(learning_rate=opts.learning_rate, n_estimators=opts.n_estimators,
-                                    subsample=ops.subsample).fit(train_feature, train_reward)
+                                    subsample=opts.subsample).fit(train_feature, train_reward)
     train_est, val_est = reg.predict(train_feature), reg.predict(val_feature)
     return train_est, val_est
 
 
 def main(opts):
     # Load the weak detector feature maps for the training and validation dataset.
-    train_feature = load_feature(opts.train_dir, opts.stage)
-    val_feature = load_feature(opts.val_dir, opts.stage)
+    train_feature = load_feature(opts.train_dir, opts.stage, size=4)
+    val_feature = load_feature(opts.val_dir, opts.stage, size=4)
     # Load the offloading rewards for the training dataset.
     train_reward = np.load(opts.label)
     assert len(train_feature) == len(
